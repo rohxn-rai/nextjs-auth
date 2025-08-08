4ca430628b6a6107ae8c6652b83e4d00ecec1c9a
rohxn-rai <rohan.rai932@outlook.com>
2025-08-08 06:06:30 -0400

    modified Homepage using session

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 import { Button } from "@heroui/react";
+import * as actions from "@/actions";
+import { auth } from "@/lib/auth";
 
-const LandingHomePage = () => {
+const LandingHomePage = async () => {
+  const session = await auth();
+
   return (
     <div>
-      <Button>Click Me!</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign in!</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign out!</Button>
+      </form>
+
+      {session?.user ? <div>Signed In</div> : <div>Signed Out</div>}
     </div>
   );
 };
